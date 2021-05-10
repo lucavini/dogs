0f5f1b90fb0284616e561b9bef6fa62454f9daf6
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-05-10 14:10:12 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Ola mundo</h1>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
