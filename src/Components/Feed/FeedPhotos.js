0f47dbd78309eb5c02fcfb28264bb3c3fb0fa4d7
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-05-28 19:37:32 -0300

    feed modal

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,29 +6,29 @@ import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "../../styles/FeedPhoto.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ pages: 1, total: 6, user: 0 });
       const { response, json } = await request(url, options);
-      console.log(json);
     }
     fetchPhotos();
   }, [request]);
 
   if (error) return <Error error={error} />;
 
-  if(loading) return <Loading />;
+  if (loading) return <Loading />;
 
-  if(data)  
-  return (
-    <ul className={ `${styles.feed} animeLeft` }>
-      {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo}/> )}
-      
-    </ul>
-  );
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
+        ))}
+      </ul>
+    );
   else return null;
 };
 
