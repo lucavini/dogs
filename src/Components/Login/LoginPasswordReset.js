29e736893937a9775496fd8a059ee17dec5e818f
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-06-04 12:30:40 -0300

    ajustes finais

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -39,7 +39,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a senha" description="" />
       <h1 className="title">Resete a senha</h1>
       <form onSubmit={handleSubmit}>
@@ -57,7 +57,7 @@ const LoginPasswordReset = () => {
       </form>
 
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
